4b365a013641daa962019ad4c69686fc645ddd94
Zuzeth Romo <zuzethromo@yahoo.com>
2019-08-15 15:10:54 -0600

    added config and image sizing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 let friends = [
     {
         name: "Betsy Ross",
-        photo: "https://www.onthisday.com/images/people/betsy-ross-medium.jpg",
+        photo: "https://www.onthisday.com/images/people/betsy-ross-medium.jpg width= 250px height= auto",
         scores: [
             "5",
             "1",
@@ -13,7 +13,7 @@ let friends = [
     }, 
     {
         name: "Katherine Langford",
-        photo: "https://media.glamour.com/photos/5aaaa5473f4cab3d54308165/master/pass/KatherineLangford-river.jpg",
+        photo: "https://media.glamour.com/photos/5aaaa5473f4cab3d54308165/master/pass/KatherineLangford-river.jpg width= 250px height= auto",
         scores: [
             "2",
             "3",
@@ -24,7 +24,7 @@ let friends = [
     }, 
     {
         name: "Michelle Obama",
-        photo: "https://media.allure.com/photos/5d0d26cbbd91b5940f740fd8/16:9/w_1280%2Cc_limit/michelle-obama-lede.jpg",
+        photo: "https://media.allure.com/photos/5d0d26cbbd91b5940f740fd8/16:9/w_1280%2Cc_limit/michelle-obama-lede.jpg width= 250px height= auto",
         scores: [
             "1",
             "4",
@@ -35,7 +35,7 @@ let friends = [
     },
     {
         name: "Lizbeth Rodriguez",
-        photo: "https://media.metrolatam.com/2019/02/25/lizbethrodriguezpublinews-49c1ec91a79ea2bb4f8656f69c751b46-900x600.jpg",
+        photo: "https://media.metrolatam.com/2019/02/25/lizbethrodriguezpublinews-49c1ec91a79ea2bb4f8656f69c751b46-900x600.jpg width= 250px height= auto",
         scores: [
             "5",
             "2",
